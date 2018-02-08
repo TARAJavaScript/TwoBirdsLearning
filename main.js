b3bc23272a53ae35e24207ce30af0a85dd248622
Tudor Hulban <prindex2010@gmail.com>
2018-02-08 18:02:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,14 +5,16 @@ tb.namespace('app.Test').set(
 
         function Test(pConfig) {
             var that = this;
+
             that.config = pConfig;
 
+            // CRUD model
             that.model = new tb.CRUD({
                 'read': {
                     url: 'fruits.json',
                     method: 'GET',
                     success: function(pResult) {
-                        that.model.data(JSON.parse(pResult.text).fruits); 
+                        that.model.data(JSON.parse(pResult.text).fruits); // -> { a: 2 }
                     },
                     error: function(pResult) {
                         console.log('an error occured', pResult);
@@ -27,6 +29,7 @@ tb.namespace('app.Test').set(
 
             // read data
             that.model.read({});
+
         }
 
         Test.prototype = {
@@ -39,10 +42,18 @@ tb.namespace('app.Test').set(
         function render(pData) {
             var that = this;
             var select = document.createElement('select');
+            var button = document.createElement('button');
+            var div = document.createElement('div');
+
+
+            $(button).html('add');
+
+            select.appendChild(document.createElement('option'));
+
 
             pData.sort(function(a, b) {
-                var textA = a.text.toUpperCase(); 
-                var textB = b.text.toUpperCase(); 
+                var textA = a.text.toUpperCase();
+                var textB = b.text.toUpperCase();
 
                 if (textA < textB) {
                     return -1;
@@ -52,23 +63,34 @@ tb.namespace('app.Test').set(
                 }
             });
 
-            $(that.target).append(select);
+
+            select = $(that.target).append(select);
+            select = $(that.target).children('select');
+
+
+            $(that.target).append(button);
+            $(that.target).append(div);
 
             pData.forEach(function createOptions(pOption) {
 
                 $('<option>')
                     .attr("value", pOption.value)
                     .html(pOption.text)
-                    .appendTo('select');
-            })
+                    .appendTo(select);
+            });
+
+
+            $(div).addClass('w3-container w3-padding-32');
+
+
         }
+
     })()
 
 );
 
 // CONSTRUCT
 new tb(
-    app.Test, 
-	{},
+    app.Test, {},
     document.body
 );
