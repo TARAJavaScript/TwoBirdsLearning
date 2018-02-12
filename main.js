56fb7628f3ad9650bc680426ed256aad03c61e8c
Tudor Hulban <prindex2010@gmail.com>
2018-02-12 12:21:12 +0200

    to_refactor_to_object

--- main.js
+++ main.js
@@ -29,12 +29,13 @@ tb.namespace('app.Test').set(
             // read data
             that.model.read({});
 
-			// create store for quantity
-			this.quantities = tb.observable({});
+            // create store for quantity
+            this.quantities = tb.observable({});
         }
 
         Test.prototype = {
             namespace: 'app.Test',
+			lineTemplate: '<div ><span>{id} - {short} - {text}</span><button>+</button>{quantity}<button>-</button><span></span></div>',
             render: render
         };
 
@@ -42,91 +43,142 @@ tb.namespace('app.Test').set(
 
         function render(pData) {
             var that = this,
-				select = document.createElement('select'),
-				button = document.createElement('button'),
-				outerdiv = document.createElement('div');
+                select = document.createElement('select'),
+                button = document.createElement('button'),
+                outerdiv = document.createElement('div');
 
-			// add an empty record as preset
+            // add an empty record as preset
             select.appendChild(document.createElement('option'));
 
-			// button caption
+            // button caption
             $(button)
-				.html('add');
+                .html('add');
 
-			// sort data for dropdown alphabetically
+            // sort data for dropdown alphabetically
             pData.sort(function(a, b) {
-				return a.text.toUpperCase() < b.text.toUpperCase() ? -1 : 1;
+                return a.text.toUpperCase() < b.text.toUpperCase() ? -1 : 1;
             });
 
-			// create dropdown entries before rendering
+            // create dropdown entries before rendering
             pData.forEach(function createOptions(pOption, pIndex) {
                 $('<option>')
                     .attr("value", pOption.value)
                     .html(pOption.text)
                     .appendTo(select);
-				pData[ pIndex ]['quantity'] = 0;
+                pData[pIndex]['quantity'] = 0;
             });
 
-			// store pData in observable
-			that.quantities( pData );
-			
-			// add some general classes
-            $(outerdiv)
-				.addClass('w3-container w3-padding-32');
+            // store pData in observable
+            that.quantities(pData);
 
-			// append all elements to document.body
+            // add some general classes
+            $(outerdiv)
+                .addClass('w3-container w3-padding-32');
+
+            // append all elements to document.body
             $(that.target)
                 .append(select)
                 .append(button)
                 .append(outerdiv);
 
-			// add selected fruit to list
-            $(button).on('click', function() {	
-			
-				// if select has a selected fruit			
-				if ( select.value ){
-					var text = $(select).children('[value="' + select.value + '"]').html(),
-						line = $( tb.parse(
-							'<div><span>{id} - {short} - {text}</span><button>+</button><button>-</button><span>{quantity}</span></div>',
-							{ 
-								id: $(select).children('[value="' + select.value + '"]').attr('value'),
-								short: $(select).children('[value="' + select.value + '"]').html()[0],
-								text: text, // see above
-								quantity: 0
-							}
-						));
-						
-					// append a line
-					$(outerdiv)
-						.append( line );	
-					
-					// add behaviour to +,- buttons
-					var buttons = Array.from( line.children('button') );	
-					
-					// add '+' functionality
-					$( buttons[0] )
-						.on(
-							'click',
-							function(){
-								// hint: "this" is the button DOM node!
-								
-								// set new data
-								that.quantities(
-									that.quantities().map(
-										function( pFruit ){
-											console.log( pFruit );
-											return pFruit.text !== text 
-												? pFruit
-												: tb.extend( pFruit, { quantity: pFruit.quantity++ } );
-										}
-									)
-								);
+            // add selected fruit to list
+            $(button).on('click', function() {
 
-								console.log( that.quantities() );
-							}
-						)
-				}                
+                // if select has a selected fruit			
+                if ( select.value ) {
+                    var text = $(select).children('[value="' + select.value + '"]').html(),
+                        id = $(select).children('[value="' + select.value + '"]').attr('value'),
+                        short = $(select).children('[value="' + select.value + '"]').html()[0],
+                        data = {
+                            id: id,
+                            short: short,
+                            text: text, // see above
+                            quantity: 1
+                        },
+                        newLine = $(tb.parse(
+                           that.lineTemplate,
+                            data
+                        )),
+                        existingLine = $('[data-id="' + data.id + '"]');
+
+                    // append a line if it doesnt exist
+                    if (!existingLine[0]) {
+	                    newLine.attr('data-id', data.id); // line select criteria
+						addButtonBehaviour( that, newLine );
+                        $(outerdiv).append( newLine );
+                    }
+
+                }
             })
+
+        }
+
+        function addButtonBehaviour( pThat, pLine ) {
+			var buttons = pLine.children();
+			
+            console.log(buttons);
+			
+            // add '+' functionality
+            $(buttons[0])
+                .on(
+                    'click',
+                    function() {
+                        // hint: "this" is the button DOM node!
+                        var q = pThat.quantities();
+						
+						console.log('+');
+                        // set new data
+                        pThat.quantities(
+                            q.map(
+                                function(pFruit) {
+                                    if (pFruit.id === pLine.attr('id')) {
+                                        pFruit.quantity++;
+                                        pLine = $(tb.parse(
+				                            pThat.lineTemplate,
+                                            pFruit
+                                        ));
+										addButtonBehaviour( pLine );
+                                        console.log(pFruit);
+                                    }
+                                    return pFruit;
+                                }
+                            )
+                        );
+
+                        console.log(pThat.quantities());
+                    }
+                );
+
+            // add '-' functionality
+            $(buttons[1])
+                .on(
+                    'click',
+                    function() {
+                        // hint: "this" is the button DOM node!
+                        var q = pThat.quantities();
+
+                        console.log('-');
+                        // set new data
+                        pThat.quantities(
+                            q.map(
+                                function(pFruit) {
+                                    if (pFruit.id === pLine.attr('id') && pFruit.quantity > 0) {
+                                        pFruit.quantity--;
+                                        pLine = $(tb.parse(
+				                            pThat.lineTemplate,
+                                            pFruit
+                                        ));
+										addButtonBehaviour( pLine );
+                                        console.log(pFruit);
+                                    }
+                                    return pFruit;
+                                }
+                            )
+                        );
+
+                        console.log(pThat.quantities());
+                    }
+                );
 
         }
 
