151c008f56ce17215861135d7a4e6cbdddbf8e3f
Tudor Hulban <prindex2010@gmail.com>
2018-03-15 13:06:00 +0200

    sync

--- 03_Stacking tB Instances/app.js
+++ 03_Stacking tB Instances/app.js
@@ -3,8 +3,30 @@ tb.namespace('app.Test').set(
     (function () {
         var $ = tb.dom;
 
+        function Test() {
+            var that = this;
+
+            that.handlers = {
+                init: render.bind(that)
+            }
+
+            Test.prototype = {
+                render: render
+            }
+
+        }
+
+
         console.log('alive');
 
+        return Test;
+
+        function render() {
+
+            var that = this;
+
+
+        }
     })()
 
 );
