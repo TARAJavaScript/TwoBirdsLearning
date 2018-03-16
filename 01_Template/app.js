521afbbb7cde45017d11bed7364d69ad1c45bb7b
Tudor Hulban <prindex2010@gmail.com>
2018-03-16 11:52:01 +0200

    Update app.js

--- 01_Template/app.js
+++ 01_Template/app.js
@@ -3,9 +3,33 @@ tb.namespace('app.Test').set(
     (function () {
         var $ = tb.dom;
 
+        function Test(pConfig) {
+
+            var that = this;
+            that.config = pConfig;
+
+            that.handlers = {
+                init: init
+            };
+        };
+
+        Test.prototype = {
+            namespace: 'app.Test',
+            render: render
+        };
+
         console.log('alive');
+
+        return Test;
+
+        function init() {
+            console.log('init');
+        };
+
+        function render() {
+            console.log('render');
+        };
 
     })()
 
 );
-
